89e2c27b5f3064b59a7b8e55483f5f12344a09ec
sebamateja <sebastian.matejasm@gmail.com>
2023-09-25 20:20:50 +0200

    Initial commit with assets

---
+++ clean.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Removing folder: Intermediate..."
+rm -rf Intermediate
+echo "Removing folder: Saved..."
+rm -rf Saved
+echo "Removing folder: Binaries..."
+rm -rf Binaries
+echo "Removing file: CrossShot.sln..."
+rm CrossShot.sln
+
+
