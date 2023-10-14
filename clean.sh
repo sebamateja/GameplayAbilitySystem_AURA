6f2da6362fc0c677d056f94e33ca439c4c7693e3
sebamateja <sebastian.matejasm@gmail.com>
2023-10-15 01:03:28 +0200

    feat: Enchanced input mapping setup
    Input action and input mapping context created.
    Fixed clean script.
    Created empty player controller for Aura.

--- clean.sh
+++ clean.sh
@@ -5,7 +5,7 @@ echo "Removing folder: Saved..."
 rm -rf Saved
 echo "Removing folder: Binaries..."
 rm -rf Binaries
-echo "Removing file: CrossShot.sln..."
-rm CrossShot.sln
+echo "Removing file: Aura.sln..."
+rm Aura.sln
 
 
