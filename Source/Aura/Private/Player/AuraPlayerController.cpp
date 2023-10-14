8ea4948f4b1ef9a6542b161a54691083d6792fef
sebamateja <sebastian.matejasm@gmail.com>
2023-10-15 01:18:45 +0200

    feat: Added mapping context to the player controller
    FInputModeGameAndUI is setup in PlayerController

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -1,5 +1,27 @@
-
-
-
 #include "Player/AuraPlayerController.h"
+#include "EnhancedInputSubsystems.h"
 
+AAuraPlayerController::AAuraPlayerController()
+{
+    bReplicates = true;
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+    Super::BeginPlay();
+
+    check(AuraContext);
+
+    UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+    check(Subsystem);
+
+    Subsystem->AddMappingContext(AuraContext, 0); // 0 -> Priority, we can have multiple MappingContexts
+
+    bShowMouseCursor = true;
+    DefaultMouseCursor = EMouseCursor::Default;
+
+    FInputModeGameAndUI InputModeData;
+    InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+    InputModeData.SetHideCursorDuringCapture(false);
+    SetInputMode(InputModeData);
+}
