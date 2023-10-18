88f68f659e6ed6e6e248c717f40c2d6c1cf05881
sebamateja <sebastian.matejasm@gmail.com>
2023-10-18 22:33:10 +0200

    feat: Tracing enemies
    Created a function to call in Tick to trace for enemies
    and highlight or unhighlight them.

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -1,11 +1,104 @@
 #include "Player/AuraPlayerController.h"
 
+#include "Interaction/TargetInterface.h"
+
 #include "EnhancedInputComponent.h"
 #include "EnhancedInputSubsystems.h"
 
 AAuraPlayerController::AAuraPlayerController()
 {
     bReplicates = true;
+}
+
+void AAuraPlayerController::PlayerTick(float DeltaTime)
+{
+    Super::PlayerTick(DeltaTime);
+
+    CursorTrace();
+}
+
+void AAuraPlayerController::CursorTrace()
+{
+    FHitResult CursorHit;
+    GetHitResultUnderCursor(ECC_Visibility, false, CursorHit);
+    if (!CursorHit.bBlockingHit) return;
+
+    LastActor = ThisActor;
+    ThisActor = Cast<ITargetInterface>(CursorHit.GetActor());
+
+    /**
+     * Line trace from cursor. There are several scenarios:
+     * A. LastActor is null && ThisActor is null
+     *      - Do nothing
+     * B. LastActor is null && ThisActor is valid
+     *      - Highlight ThisActor
+     * C. LastActor is valid && ThisActor is null
+     *      - UnHighlight LastActor
+     * D. Both actors are valid, but LastActor != ThisActor
+     *      - UnHighlight LastActor
+     *      - Highlight ThisActor
+     * E. Both actors are valid, and are the same actor
+     *      - Do nothing
+     */
+
+    if (LastActor == nullptr)
+    {
+        if (ThisActor != nullptr)
+        {
+            // Case B
+            ThisActor->HighlightActor();
+        }
+        else
+        {
+            // Case A - both are null, do nothing
+        }
+    }
+    else // LastActor is valid
+    {
+        if (ThisActor == nullptr)
+        {
+            // Case C
+            LastActor->UnHighlightActor();
+        }
+        else // both actors are valid
+        {
+            if (LastActor != ThisActor)
+            {
+                // Case D
+                LastActor->UnHighlightActor();
+                ThisActor->HighlightActor();
+            }
+            else
+            {
+                // Case E - do nothing
+            }
+        }
+    }
+
+
+    // CASE A and E - Both actors are the same, either null or valid - do nothing
+    // if (LastActor == ThisActor)
+    // {
+    //     return;
+    // }
+    // // CASE C
+    // if (LastActor && ThisActor == nullptr)
+    // {
+    //     LastActor->UnHighlightActor();
+    //     return;
+    // }
+    // // CASE B
+    // if (LastActor == nullptr && ThisActor)
+    // {
+    //     ThisActor->HighlightActor();
+    //     return;
+    // }
+    // // CASE D
+    // if (LastActor != ThisActor)
+    // {
+    //     LastActor->UnHighlightActor();
+    //     ThisActor->HighlightActor();
+    // }
 }
 
 void AAuraPlayerController::BeginPlay()
