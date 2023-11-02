2cc92d8930f43f59faadb00023307341268207ce
sebamateja <sebastian.matejasm@gmail.com>
2023-11-02 22:23:43 +0100

    feat: Constructing AbilitySystemComponent and AttributeSet for player and enemies feat: Used interface IAbilitySystemInterface

--- Source/Aura/Private/Player/AuraPlayerState.cpp
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -1,11 +1,24 @@
 #include "Player/AuraPlayerState.h"
 
+#include "AbilitySystem/AuraAbilitySystemComponent.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+
 AAuraPlayerState::AAuraPlayerState()
 {
+    AbilitySystemComponent = CreateDefaultSubobject<UAuraAbilitySystemComponent>("AbilitySystemComponent");
+    AbilitySystemComponent->SetIsReplicated(true);
+    
+    AttributeSet = CreateDefaultSubobject<UAuraAttributeSet>("AttributeSet");
+
     // How often the server will try to update clients
     // So as changes occur on the server for the player state, then the server
     // will be sending updates out to all clients so that they cann sync up with the server version
     // Default value for Player State to receive update is about 500
     // Other games like Lyra or Fortnite are using more frequent updates (about 100)
     NetUpdateFrequency = 100.0;
+}
+
+UAbilitySystemComponent* AAuraPlayerState::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
 }
