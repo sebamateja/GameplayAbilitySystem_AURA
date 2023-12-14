eff222d80601438ad9268f9982452d72b6ee31bc
sebamateja <sebastian.matejasm@gmail.com>
2023-12-14 18:59:47 +0100

    feat: Created custom AssetManager class and set it for the entire project feat: Calling InitializeNativeGameplayTags method in AssetManager class dosc: Update about AssetManager

--- Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
@@ -1,9 +1,13 @@
 #include "AbilitySystem/AuraAbilitySystemComponent.h"
-
+#include "AuraGameplayTags.h"
 
 void UAuraAbilitySystemComponent::AbilityActorInfoSet()
 {
     OnGameplayEffectAppliedDelegateToSelf.AddUObject(this, &UAuraAbilitySystemComponent::EffectApplied);
+
+    //Example how we can access GameplayTags from FAuraGameplayTag singleton
+    // const FAuraGameplayTags& GameplayTags = FAuraGameplayTags::Get();
+    // GameplayTags.Attributes_Secondary_Armor.ToString();
 }
 
 void UAuraAbilitySystemComponent::EffectApplied(UAbilitySystemComponent* AbilitySystemComponent, 
