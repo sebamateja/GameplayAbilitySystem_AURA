d778514dd10b3fd6e764e20aaa6ea48f36086faa
sebamateja <sebastian.matejasm@gmail.com>
2023-12-19 20:20:59 +0100

    feat: Created DataAsset class for Input Configuration with array of struct InputAction to Tag feat: Created InputAction blueprints for 1,2,3,4,LMB,RMB feat: Added InputActions in ContextMapping feat: Created tags for InputTags feat: Added InputActions and InputTags to DataAsset InputConfiguration

---
+++ Source/Aura/Private/Input/AuraInputConfig.cpp
@@ -0,0 +1,18 @@
+#include "Input/AuraInputConfig.h"
+
+const UInputAction* UAuraInputConfig::FindAbilityInputActionForTag(const FGameplayTag& InputTag, bool bLogNotFound) const
+{
+    for (const FAuraInputAction& Action : AbilityInputActions)
+    {
+        if (Action.InputAction && Action.InputTag == InputTag)
+        {
+            return Action.InputAction;
+        }
+    }
+
+    if (bLogNotFound)
+    {
+        UE_LOG(LogTemp, Error, TEXT("Cant find AbilityInputAction for InputTag [%s], on InputConfig [%s]"), *InputTag.ToString(), *GetNameSafe(this));
+    }
+    return nullptr;
+}
