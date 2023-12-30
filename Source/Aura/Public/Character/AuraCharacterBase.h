5b57a88dc7e61dcb9d6842009460c078870a0440
sebamateja <sebastian.matejasm@gmail.com>
2023-12-30 18:12:37 +0100

    feat: Added dissolve effect to Goblin enemies feat: Added function in AuraCharacterBase to change materials to dissolve ones when enemy is dying feat: Implementing timeline to change parameter in dynamic material in blueprint using BlueprintImplementableEvent

--- Source/Aura/Public/Character/AuraCharacterBase.h
+++ Source/Aura/Public/Character/AuraCharacterBase.h
@@ -61,6 +61,22 @@ protected:
 
 	void AddCharacterAbilities();
 
+	/* Dissolve Effect */
+	void Dissolve();
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void StartDissolveTimeline(UMaterialInstanceDynamic* DynamicMaterialInstance);
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void StartWeaponDissolveTimeline(UMaterialInstanceDynamic* DynamicMaterialInstance);
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TObjectPtr<UMaterialInstance> DissolveMaterialInstance;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TObjectPtr<UMaterialInstance> WeaponDissolveMaterialInstance;
+	/* Dissolve Effect Ends */
+
 private:
 	UPROPERTY(EditAnywhere, Category = "Abilities")
 	TArray<TSubclassOf<UGameplayAbility>> StartupAbilities;
