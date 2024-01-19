e4d4697e9b977367f53d15ab2e240207f9fca496
sebamateja <sebastian.matejasm@gmail.com>
2024-01-19 22:51:53 +0100

    fix: Fixed crash in AuraProjectile.cpp. Wrong condition check feat: Added dissolve effect to the Shaman enemy and its staff weapon

--- Source/Aura/Private/Actor/AuraProjectile.cpp
+++ Source/Aura/Private/Actor/AuraProjectile.cpp
@@ -41,7 +41,7 @@ void AAuraProjectile::BeginPlay()
 // This happens on the server
 void AAuraProjectile::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	if (DamageEffectSpecHandle.Data.IsValid() && DamageEffectSpecHandle.Data.Get()->GetContext().GetEffectCauser() == OtherActor)
+	if (!DamageEffectSpecHandle.Data.IsValid() || DamageEffectSpecHandle.Data.Get()->GetContext().GetEffectCauser() == OtherActor)
 	{
 		return;
 	}
