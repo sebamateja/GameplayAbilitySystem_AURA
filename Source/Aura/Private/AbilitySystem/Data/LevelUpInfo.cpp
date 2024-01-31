00dab5c6989e7992865dea7493c1ff828b4c1272
sebamateja <sebastian.matejasm@gmail.com>
2024-01-31 20:34:42 +0100

    feat: Created struct FAuraLevelUpInfo that keeps xp requirement, attribute point       award and spell point award per level feat: Created DataAsset blueprint for it

---
+++ Source/Aura/Private/AbilitySystem/Data/LevelUpInfo.cpp
@@ -0,0 +1,23 @@
+#include "AbilitySystem/Data/LevelUpInfo.h"
+
+int32 ULevelUpInfo::FindLevelForXP(float XP)
+{
+    int32 Level = 1;
+    bool bSearching = true;
+    while (bSearching)
+    {
+        // LevelUpInformation[1] = Level 1 information
+        // LevelUpInformation[2] = Level 2 information
+        if (LevelUpInformation.Num() - 1 <= Level) return Level;
+
+        if (XP >= LevelUpInformation[Level].LevelUpRequirement)
+        {
+            ++Level;
+        }
+        else
+        {
+            bSearching = false;
+        }
+    }
+    return Level;
+}
