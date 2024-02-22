64d081a9e548b9da1ab32d64db0804d3c429456a
sebamateja <sebastian.matejasm@gmail.com>
2024-02-22 19:23:55 +0100

    feat(280): Handling EquippedRowButton to handle AbilityInfo delegate feat: Added 2 new tags for passive ability feat: Setting up widget controllers in WBP_EquippedSpellRow and its subwidgets

--- Source/Aura/Private/AuraGameplayTags.cpp
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -96,6 +96,12 @@ void FAuraGameplayTags::InitializeNativeGameplayTags()
 	GameplayTags.InputTag_4 = UGameplayTagsManager::Get().AddNativeGameplayTag(
 		FName("InputTag.4"),
 		FString("Input Tag for 4 key"));
+	GameplayTags.InputTag_Passive_1 = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("InputTag.Passive.1"),
+		FString("Input Tag for Passive Ability 1"));
+	GameplayTags.InputTag_Passive_2 = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("InputTag.Passive.2"),
+		FString("Input Tag for Passive Ability 1"));
 
 	/**
 	 * Event Tags
--- Source/Aura/Private/UI/WidgetController/SpellMenuWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/SpellMenuWidgetController.cpp
@@ -2,7 +2,7 @@
 
 void USpellMenuWidgetController::BroadcastInitialValues()
 {
-
+    BroadcastAbilityInfo();
 }
 
 void USpellMenuWidgetController::BindCallbacksToDependencies()
