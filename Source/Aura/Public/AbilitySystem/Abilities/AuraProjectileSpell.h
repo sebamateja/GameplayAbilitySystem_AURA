8cb66e2d37c1f5a77777c10bb13e40104c16ee5a
sebamateja <sebastian.matejasm@gmail.com>
2024-04-05 20:38:40 +0200

    feat(293): FireBolt Description feat: Overriding description funtions in AuraProjectileSpell for FireBolt

--- Source/Aura/Public/AbilitySystem/Abilities/AuraProjectileSpell.h
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraProjectileSpell.h
@@ -13,6 +13,10 @@ class AURA_API UAuraProjectileSpell : public UAuraDamageGameplayAbility
 {
 	GENERATED_BODY()
 	
+public:
+	virtual FString GetDescription(int32 Level) override;
+	virtual FString GetNextLevelDescription(int32 Level) override;
+
 protected:
 	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
 
@@ -21,4 +25,7 @@ protected:
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly)
 	TSubclassOf<AAuraProjectile> ProjectileClass;
+
+	UPROPERTY(EditDefaultsOnly)
+	int32 NumberOfProjectiles = 5;
 };
