0569118a6d0df9a39e9bf601d917428d8f485f33
sebamateja <sebastian.matejasm@gmail.com>
2024-04-15 22:48:34 +0200

    feat(315): FireBolt Projectile Spread feat: Added logic for spawning multiple fire bolts projectile in spread

--- Source/Aura/Private/AbilitySystem/Abilities/FireBolt.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/FireBolt.cpp
@@ -1,5 +1,39 @@
 #include "AbilitySystem/Abilities/FireBolt.h"
 
+#include "Kismet/KismetSystemLibrary.h" 
+
+void UFireBolt::SpawnProjectiles(const FVector& ProjectileTargetLocation, const FGameplayTag& SocketTag, bool bOverridePitch, float PitchOverride, AActor* HomingTarget)
+{
+    const bool bIsServer = GetAvatarActorFromActorInfo()->HasAuthority();
+    if (!bIsServer) return;
+
+    const FVector SocketLocation = ICombatInterface::Execute_GetCombatSocketLocation(
+        GetAvatarActorFromActorInfo(), SocketTag);
+    
+    FRotator Rotation = (ProjectileTargetLocation - SocketLocation).Rotation();
+    if (bOverridePitch) Rotation.Pitch = PitchOverride;
+
+    const FVector Forward = Rotation.Vector();
+    const FVector LeftOfSpread = Forward.RotateAngleAxis(-ProjectileSpread / 2.0f, FVector::UpVector);
+    const FVector RightOfSpread = Forward.RotateAngleAxis(ProjectileSpread / 2.0f, FVector::UpVector);
+
+    NumberOfProjectiles = FMath::Min(MaxNumProjectiles, GetAbilityLevel());
+    
+    if (NumberOfProjectiles > 1)
+    {
+        const float DeltaSpread = ProjectileSpread / (NumberOfProjectiles - 1);
+        for (int32 i = 0; i < NumberOfProjectiles; i++)
+        {
+            const FVector Direction = LeftOfSpread.RotateAngleAxis(DeltaSpread * i, FVector::UpVector);
+            UKismetSystemLibrary::DrawDebugArrow(GetAvatarActorFromActorInfo(), SocketLocation, SocketLocation + Direction * 100.0f, 4.0f, FLinearColor::Green, 3.0f);
+        }
+    }
+    else
+    {
+        // Single projectile
+    }
+}
+
 FString UFireBolt::GetDescription(int32 Level)
 {
     const int32 ScaledDamage = Damage.GetValueAtLevel(Level);
--- Source/Aura/Public/AbilitySystem/Abilities/FireBolt.h
+++ Source/Aura/Public/AbilitySystem/Abilities/FireBolt.h
@@ -11,4 +11,15 @@ class AURA_API UFireBolt : public UAuraProjectileSpell
 public:
 	virtual FString GetDescription(int32 Level) override;
 	virtual FString GetNextLevelDescription(int32 Level) override;
+
+	UFUNCTION(BlueprintCallable)
+	void SpawnProjectiles(const FVector& ProjectileTargetLocation, const FGameplayTag& SocketTag, bool bOverridePitch, float PitchOverride, AActor* HomingTarget);
+
+protected:
+
+	UPROPERTY(EditDefaultsOnly, Category = "Custom|Firebolt")
+	float ProjectileSpread = 90.0f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Custom|Firebolt")
+	int32 MaxNumProjectiles = 5;
 };
