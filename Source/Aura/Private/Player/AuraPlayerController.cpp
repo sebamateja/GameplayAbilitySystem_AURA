9fa5509de45f4dc53d1155d7d671f5cef74d0f03
sebamateja <sebastian.matejasm@gmail.com>
2024-07-01 22:51:39 +0200

    feat(358): Ignore Enemies while Magic Circle Active feat: Created new Object Channel: ExcludePlayers feat: Not highlighting players/enemies when MagicCircle is active       by ignoring ExcludePlayers trace channel

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -55,7 +55,8 @@ void AAuraPlayerController::CursorTrace()
         ThisActor = nullptr;
         return;
     }
-    GetHitResultUnderCursor(ECC_Visibility, false, CursorHit);
+    const ECollisionChannel TraceChannel = IsValid(MagicCircle) ? ECC_ExcludePlayers : ECC_Visibility;
+    GetHitResultUnderCursor(TraceChannel, false, CursorHit);
     if (!CursorHit.bBlockingHit) return;
 
     LastActor = ThisActor;
